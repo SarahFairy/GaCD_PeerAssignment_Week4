0f2b625972285b5ae14ab473b0ecc2d9815aa1e0
Sarah-Fee <148388837+SarahFairy@users.noreply.github.com>
2023-11-29 14:04:49 +0100

    Coursera submission files added
    added README, CodeBook, R code and tidy dataset

---
+++ README.md
@@ -0,0 +1,42 @@
+==================================================================
+
+The script "run_analysis.R" assumes that the experiment data is downloaded to your computer. To make this script work, download the data and unzip it. Set your working directory to the parent folder of the downloaded data: setwd("/YOURPATH/UCI HAR Dataset"). Do not move any of the folders or files within "UCI HAR Dataset". Now, the code can be executed.
+
+The code sets separate variables for the working directory "wd" and for the folders where test and train data is saved ("path_test" and "path_train", respectively).
+
+It contains two functions:
+1. label_activity: This function converts the activity number to the activity name by using "activity_labels.txt".
+2. replace_space: This function replaces spaces and dashes in strings by underscores.
+
+After introducing the functions, the "features.txt" file is imported. This file contains the column names. It is imported as a data frame, then the replace_space function is applied and the list is converted to a character vector "col_names_str".
+
+The code does the following for both test and train data:
+1. Read the activity labels from "y_test/train.txt" and convert the activity number to the actual activity name. It then converts the list to a data frame and re-names the column to "Activity".
+2. Read the subjects from "subject_test/train.txt" and re-name the column to "Subject".
+3. Read the results from "X_test/train.txt" and changes the columns V1, V2, ... to the above created character vector "col_names_str".
+4. First, the activity names are added to the left of the results dataset and after that, the subjects are added to the left as well.
+
+Once the above steps have been executed for both test and train data, both data frames are merged to one data frame "res". After that, the environment is cleaned up by removing large variables that are not required anymore.
+
+The dataset "res" is then filtered on the columns "Subject", "Activity" and all columns that contain either "mean()" or "std()". It is explicitly filtering for these names, i.e. column names with "mean" in the middle of their names are filtered out. If necessary, this can be changed by omitting "()" in the "contains" statements.
+
+To create the tidy dataset "res_summary", the data frame is grouped by "Subject" and "Activity" and the mean()-function is applied to all columns in the data frame. Now, this tidy dataset contains for each Subject-Activity combination the mean of all measurements that are showing the mean or standard deviation.
+
+After tidying up the column names, the final dataset is written to a txt file in the working directory "wd" and it can be re-imported by: 
+df_result <- read.table(file.path(wd, "summary_subj_activity.txt"), header = TRUE, check.names = FALSE)
+
+
+
+The dataset includes the following files:
+=========================================
+
+- 'README.md'
+
+- 'CodeBook.md': Contains information about the variables used in the tidy dataset
+
+- 'run_analysis.R': R code to produce the tidy dataset
+
+- 'summary_subj_activity.txt': Tidy dataset
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,96 @@
+## Set directories
+# Set working directory
+# setwd("/YOURPATH/UCI HAR Dataset")
+wd <- getwd()
+
+# Get file paths for test and train folders that contain required data
+path_test <- file.path(wd, "test")
+path_train <- file.path(wd, "train")
+
+## Functions
+# function that uses the activity_labels.txt and overwrites the label to the activity name
+activity <- read.table(file.path(wd, "activity_labels.txt"), header = FALSE)
+label_activity <- function(x) {activity[x,2]}
+# function that replaces space and dash by underscore
+replace_space <- function(x) str_replace_all(x, c(" "="_", "-"="_"))
+
+### Import 
+# import features.txt which contains column names
+col_names <- read.delim(file.path(wd, "features.txt"), header = FALSE)
+col_names <- lapply(col_names[,"V1"], replace_space)
+# convert list to a character vector
+col_names_str <- unlist(col_names)
+
+## Import of test data
+# import activity labels of tests
+test_label <- read.delim(file.path(path_test, "y_test.txt"), header = FALSE)
+# replace the number of activity by the activity name
+test_label <- lapply(test_label[,"V1"], label_activity)
+# convert the list to a dataframe and re-name column
+test_label <- do.call(rbind.data.frame, test_label)
+names(test_label) <- "Activity"
+
+# import subjects of tests and re-name column
+test_subj <- read.delim(file.path(path_test, "subject_test.txt"), header = FALSE)
+names(test_subj) <- "Subject"
+
+# import results from tests
+test <- read.table(file.path(path_test, "X_test.txt"), header = FALSE)
+# re-name the columns of test data set by using string created from features file
+names(test) <- col_names_str
+
+# merge the activity and subjects to the left of the results table
+test <- cbind(test_label, test)
+test <- cbind(test_subj, test)
+
+
+## Import of training data
+# import activity labels of training
+train_label <- read.delim(file.path(path_train, "y_train.txt"), header = FALSE)
+# replace the number of activity by the activity name
+train_label <- lapply(train_label[,"V1"], label_activity)
+# convert the list to a dataframe and re-name column
+train_label <- do.call(rbind.data.frame, train_label)
+names(train_label) <- "Activity"
+
+# import subjects of training and re-name column
+train_subj <- read.delim(file.path(path_train, "subject_train.txt"), header = FALSE)
+names(train_subj) <- "Subject"
+
+# import results from training 
+train <- read.table(file.path(path_train, "X_train.txt"), header = FALSE)
+# re-name the columns of train data set by using string created from features file
+names(train) <- col_names_str
+
+# merge the activity and subjects on the left of the results table
+train <- cbind(train_label, train)
+train <- cbind(train_subj, train)
+
+# merge test and train together
+res <- rbind(test, train)
+
+# clean up environment
+rm(test, train, test_label, train_label, test_subj, train_subj)
+
+# only filter for columns "Subject" and "Activity" and those that contain "mean()" or "std()"
+# no filtering on columns that contain "mean" or "std" in the middle of the name
+res <- select(res, contains("Subject") | contains("Activity") | contains("mean()") | contains("std()"))
+
+# group the result by subject and activity and then take mean of every column by group
+res_summary <- res %>%
+  group_by(Subject,Activity) %>%
+  summarise_all(mean)
+
+# get rid off the numbers in front of column names
+final_col <- names(res_summary)
+final_col <- sub("[0-9]+_", "", final_col)
+names(res_summary) <- final_col
+
+# write summary dataframe to txt file
+write.table(res_summary, file.path(wd, "summary_subj_activity.txt"), col.names = TRUE)
+
+# import of written txt file
+# df_result <- read.table(file.path(wd, "summary_subj_activity.txt"), header = TRUE, check.names = FALSE)
+
+
+
